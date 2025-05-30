f3986372bb842c7772e921d266bd34dffd5aaf4a
Henritraficca <gabrielhenriquemariano2006@gmail.com>
2025-05-30 22:15:58 +0000

    dei inicio ao codigo e fiz toda a entradad de dados da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+    //PontosT = pontos turisticos
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+        int main() {
+            char estado1[50], codigo1[50], cidade1[50], estado2[50], codigo2[50], cidade2[50];
+            int populacao1, pontosT1, populacao2, pontosT2;
+            float pib1, area1, pib2, area2;
 
-    return 0;
+            printf("Insira os dados da sua carta!\n");
+            printf("carta 1!\n");
+           
+            printf("qual é o estado da sua carta?: \n");
+            scanf("%s", &estado1);
+
+            printf("qual é o codigo?: \n");
+            scanf("%s", &codigo1);
+
+            printf("Digite o nome da cidade:\n");
+            scanf("%s", &cidade1);
+
+            printf("qual a população da sua cidade?:\n");
+            scanf("%d", &populacao1);
+
+            printf("digite a área da sua cidade:\n");
+            scanf("%f", &area1);
+
+            printf("quanto é o PIB?:\n");
+            scanf("%f", &pib1);
+
+            printf("e pra finalizar, digite aqui a quantidade de pontos turisticos:\n");
+            scanf("%d", &pontosT1);
+
+//tentei usar como exemplo em um teste a cidade de São Paulo, mas eu acho que por ser um nome composto com um espaço no meio não funcionou
+//respostas de string com espaço no meio quebram o código :/
+
+            printf("");
+            
+
+
+            
+
 }
