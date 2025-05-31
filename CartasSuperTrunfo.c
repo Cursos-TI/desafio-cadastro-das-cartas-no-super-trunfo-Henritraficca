6ce047f00217e2b43619c56bc5c08b00f593822a
Henritraficca <gabrielhenriquemariano2006@gmail.com>
2025-05-31 00:24:42 +0000

    eu terminei a primeira carta, entrada e saida de dados estão funcionando como deveriam

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
     //PontosT = pontos turisticos
+    //eu dividi os floats das áreas e dos pibs, pq tava dando uma confusão entre eles e eu não tava entendendo bem ai pensei que assim resolveria
 
         int main() {
             char estado1[50], codigo1[50], cidade1[50], estado2[50], codigo2[50], cidade2[50];
             int populacao1, pontosT1, populacao2, pontosT2;
-            float pib1, area1, pib2, area2;
+            float area1, area2;
+            float pib1, pib2;
 
             printf("Insira os dados da sua carta!\n");
             printf("carta 1!\n");
@@ -34,7 +36,13 @@
 //tentei usar como exemplo em um teste a cidade de São Paulo, mas eu acho que por ser um nome composto com um espaço no meio não funcionou
 //respostas de string com espaço no meio quebram o código :/
 
-            printf("");
+            printf("Carta 1:\n");
+            printf("Estado: %s\n", estado1);
+            printf("Código: %s\n", codigo1);
+            printf("Nome da cidade: %s\nPopulação %d\n", cidade1, populacao1);
+            printf("Área: %f\nPIB: %f\n", area1, pib1);
+            printf("Pontos turisticos: %d\n", pontosT1);
+
             
 
 
