8268c634c9b0b78d94476d44b64f5c26f1a7bd2d
Henritraficca <gabrielhenriquemariano2006@gmail.com>
2025-06-01 14:30:31 +0000

    terminei o codigo, agora vou tentar corrigir algumas falhas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,25 +12,25 @@
             printf("Insira os dados da sua carta!\n");
             printf("carta 1!\n");
            
-            printf("qual é o estado da sua carta?: \n");
+            printf("qual é o estado da sua carta?: ");
             scanf("%s", &estado1);
 
-            printf("qual é o codigo?: \n");
+            printf("qual é o codigo?: ");
             scanf("%s", &codigo1);
 
-            printf("Digite o nome da cidade:\n");
+            printf("Digite o nome da cidade: ");
             scanf("%s", &cidade1);
 
-            printf("qual a população da sua cidade?:\n");
+            printf("qual a população da sua cidade?: ");
             scanf("%d", &populacao1);
 
-            printf("digite a área da sua cidade:\n");
+            printf("digite a área da sua cidade: ");
             scanf("%f", &area1);
 
-            printf("quanto é o PIB?:\n");
+            printf("quanto é o PIB?: ");
             scanf("%f", &pib1);
 
-            printf("e pra finalizar, digite aqui a quantidade de pontos turisticos:\n");
+            printf("e pra finalizar, digite aqui a quantidade de pontos turisticos: ");
             scanf("%d", &pontosT1);
 
 //tentei usar como exemplo em um teste a cidade de São Paulo, mas eu acho que por ser um nome composto com um espaço no meio não funcionou
@@ -43,9 +43,44 @@
             printf("Área: %f\nPIB: %f\n", area1, pib1);
             printf("Pontos turisticos: %d\n", pontosT1);
 
-            
+//finalizado o cadastro da primeira carta, agora o código da segunda
+
+            printf("Agora vamos cadastrar a sua segunda carta!\n");
+            printf("Carta 2:\n");
+
+            printf("Qual é o estado da sua carta?: ");
+            scanf("%s", &estado2);
+
+            printf("Qual é o codigo? ");
+            scanf("%s", &codigo2);
+
+            printf("Digite o nome da cidade: ");
+            scanf("%s", &cidade2);
+
+            printf("Qual a população da cidade?: ");
+            scanf("%d", &populacao2);
+
+            printf("Digite a area da sua cidade: ");
+            scanf("%f", &area2);
+
+            printf("Quanto é o pib?: ");
+            scanf("%f", &pib2);
+
+            printf("E pra finalizar, digite aqui a quantidade de pontos turisticos: ");
+            scanf("%d", &pontosT2);
+
+            printf("carta 2:\n");
+            printf("Estado: %s\n", estado2);
+            printf("Código: %s\n", codigo2);
+            printf("Nome da cidade: %s\nPopulação: %d\n", cidade2, populacao2);
+            printf("Área: %f\nPIB: %f\n", area2, pib2);
+            printf("Pontos turisticos: %d", pontosT2);
 
 
-            
+          return 0;              
+
+//eu terminei de escrever todo o código e até ta rodando tudo como deveria, mas ainda estão acontecendo alguns bugs e eu sinceramente não consegui entender o motivo
+//vou tentar mudar alguma coisa nas váriaveis e talvez fique meio estranho mas o importante é rodar tudo certo
 
 }
+
